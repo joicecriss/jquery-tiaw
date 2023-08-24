9ef6a5adffca0fd657cae29ea8d3509b0ee335cb
Joice Cristina <71676439+joicecriss@users.noreply.github.com>
2023-08-23 22:27:18 -0300

    ajustando detalhes do css

--- scripts/global.js
+++ scripts/global.js
@@ -1,75 +1,75 @@
 // exercicio 1
 
 $(document).ready(function(){
-    $("#texto").hide();
-    $("#exc1").click(function(){
-      $("#texto").show();
-    });
+  $("#texto").hide();
+  $("#exc1").click(function(){
+    $("#texto").show();
+  });
 });
 
 // exercicio 2
 
 $(document).ready(function(){
-    $("#exc2").click(function(){
-      $(".nome").toggle();
-    });
+  $("#exc2").click(function(){
+    $(".nome").toggle();
+  });
 });
 
 //exercicio 3
 
 $(document).ready(function(){
-    $(".sublista").hide();
-    $(".lista").click(function(){
-      $(".sublista").slideToggle();
-    });
+  $(".sublista").hide();
+  $(".lista").click(function(){
+    $(".sublista").slideToggle();
+  });
 });
 
 // exercicio 4
 
 $(document).ready(function(){
-    $("#exc4").click(function(){
-        var name = $("#name").val();
-        var address = $("#address").val();
+  $("#exc4").click(function(){
+      var name = $("#name").val();
+      var address = $("#address").val();
 
-        var errorname = "";
-        var erroraddress = "";
+      var errorname = "";
+      var erroraddress = "";
 
-        if(name.length < 5) {
-            errorname += "O nome deve conter mais de 4 caracteres.";
-        } else if(name.length > 40) {
-            errorname += "O nome deve conter menos de 41 caracteres.";
-        } else if (name.split(" ") >= 0) {
-            errorname += "O nome não pode estar vazio."
-        }
+      if(name.length < 5) {
+          errorname += "O nome deve conter mais de 4 caracteres.";
+      } else if(name.length > 40) {
+          errorname += "O nome deve conter menos de 41 caracteres.";
+      } else if (name.split(" ") >= 0) {
+          errorname += "O nome não pode estar vazio."
+      }
 
-        if(address.split(" ").length < 2) {
-            erroraddress += "O endereço não pode estar vazio e deve conter duas palavras";
-        }
+      if(address.split(" ").length < 2) {
+          erroraddress += "O endereço não pode estar vazio e deve conter duas palavras";
+      }
 
-        $("#errorname").text(errorname).show("slow");
-        $("#erroraddress").text(erroraddress).show("slow");
+      $("#errorname").text(errorname).show("slow");
+      $("#erroraddress").text(erroraddress).show("slow");
 
-    })
+  })
 })
 
 //exercicio 5
 
 $(document).ready(function() {
-    var contSlide = 0;
-    var totalSlides = $('#carousel img').length;
-    
-    $('.btnNext').click(function() {
-      contSlide = (contSlide + 1) % totalSlides;
-      updateSlide();
-    });
-    
-    $('.btnPrev').click(function() {
-      contSlide = (contSlide - 1 + totalSlides) % totalSlides;
-      updateSlide();
-    });
-    
-    function updateSlide() {
-      $('#carousel img').hide();
-      $('#carousel img:eq(' + contSlide + ')').show();
-    }
+  var constSlide = 0;
+  var totalSlides = $('#carousel img').length;
+  
+  $('.btnNext').click(function() {
+    constSlide = (constSlide + 1) % totalSlides;
+    updateSlide();
+  });
+  
+  $('.btnPrev').click(function() {
+    constSlide = (constSlide - 1 + totalSlides) % totalSlides;
+    updateSlide();
+  });
+  
+  function updateSlide() {
+    $('#carousel img').fadeTo("slow", 1).hide();
+    $('#carousel img:eq(' + constSlide + ')').fadeOut("slow", 3000).show();
+  }
 });
